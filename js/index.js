7298cf5e7e0c865fcfa90f5e8c55cdeb37dd895e
GitNes26 <nestorpuentesin@gmail.com>
2022-10-24 16:02:05 -0500

    se agrego animacion de giro en el hero

--- js/index.js
+++ js/index.js
@@ -1,48 +1,45 @@
 AOS.init();
 
-let timeTimeOut = 4100;
+let timeTimeOut = 3500;
 let time = 300;
 
 const animation_1 = () => {
    document.querySelector("#intro_logo").classList.add("d-none");
-   setTimeout(() => {}, 100)
 }
-const animation_2 = async() => {
+const animation_2 = () => {
    document.querySelector("#intro_1").classList.remove("d-none");
-   setTimeout(() => {}, time)
 }
-const animation_3 = async() => {
+const animation_3 = () => {
    document.querySelector("#intro_1").classList.add("d-none");
    document.querySelector("#intro_2").classList.remove("d-none");
-   setTimeout(() => {}, time)
 }
-const animation_4 = async() => {
+const animation_4 = () => {
    document.querySelector("#intro_2").classList.add("d-none");
    document.querySelector("#intro_3").classList.remove("d-none");
    time = 2600;
-   setTimeout(() => {}, time)
 }
-const animation_5 = async() => {
+const animation_5 = () => {
    document.querySelector("#intro_3").classList.add("fade_out");
    document.querySelector("#hero").classList.remove("d-none");
    time = 600;
-   setTimeout(() => {}, time)
 }
-const animation_6 = async() => {
+const animation_6 = () => {
    document.querySelector("#intro_3").classList.add("d-none");
    document.querySelector("#hero").classList.add("fade_in");
-   setTimeout(() => {}, time)
+   document.querySelector("#img_hero_1").classList.add("img_rotate_right");
+   document.querySelector("#img_hero_2").classList.add("img_rotate_left");
 }
-// const animations = async() => {
-//    await animation_1();
-//    await animation_2();
-//    await animation_3();
-//    await animation_4();
-//    await animation_5();
-//    await animation_6();
-// }
+const animation_7 = () => {
+   document.querySelector("#span_icon").classList.remove("d-none");
+   document.querySelector("body").classList.remove("scroll-y-none");
+}
+
 
 document.addEventListener("DOMContentLoaded", function() {
+   document.querySelector("body").classList.add("scroll-y-none");
+   document.querySelector("#img_hero_1").classList.remove("img_rotate_right");
+   document.querySelector("#img_hero_2").classList.remove("img_rotate_left");
+
    const animations = () => {
       setTimeout(() => animation_1(), timeTimeOut);
       setTimeout(() => animation_2(), timeTimeOut += time);
@@ -50,6 +47,7 @@ document.addEventListener("DOMContentLoaded", function() {
       setTimeout(() => animation_4(), timeTimeOut += time);
       setTimeout(() => animation_5(), timeTimeOut += time);
       setTimeout(() => animation_6(), timeTimeOut += time);
+      setTimeout(() => animation_7(), timeTimeOut += time);
    }
    animations();
 })
