3d37ed6f51bc279ac0046ef7268d0aea82c53464
GitNes26 <nestorpuentesin@gmail.com>
2022-10-31 13:20:15 -0600

    se cambio el efcto de transicion en los logos del hero movimientos horizontales de entrada

--- js/index.js
+++ js/index.js
@@ -5,31 +5,14 @@ let time = 300;
 
 const animation_1 = () => {
    document.querySelector("#intro_logo").classList.add("d-none");
+   // document.querySelector("#hero").classList.remove("d-none");
 }
 const animation_2 = () => {
-   document.querySelector("#intro_1").classList.remove("d-none");
+   document.querySelector("#hero").classList.add("fade_in");
+   document.querySelector("#img_hero_1").classList.add("img_left_right");
+   document.querySelector("#img_hero_2").classList.add("img_right_left");
 }
 const animation_3 = () => {
-   document.querySelector("#intro_1").classList.add("d-none");
-   document.querySelector("#intro_2").classList.remove("d-none");
-}
-const animation_4 = () => {
-   document.querySelector("#intro_2").classList.add("d-none");
-   document.querySelector("#intro_3").classList.remove("d-none");
-   time = 2600;
-}
-const animation_5 = () => {
-   document.querySelector("#intro_3").classList.add("fade_out");
-   document.querySelector("#hero").classList.remove("d-none");
-   time = 600;
-}
-const animation_6 = () => {
-   document.querySelector("#intro_3").classList.add("d-none");
-   document.querySelector("#hero").classList.add("fade_in");
-   document.querySelector("#img_hero_1").classList.add("img_rotate_right");
-   document.querySelector("#img_hero_2").classList.add("img_rotate_left");
-}
-const animation_7 = () => {
    document.querySelector("#span_icon").classList.remove("d-none");
    document.querySelector("body").classList.remove("scroll-y-none");
 }
@@ -38,16 +21,12 @@ const animation_7 = () => {
 document.addEventListener("DOMContentLoaded", function() {
    document.querySelector("body").classList.add("scroll-y-none");
    document.querySelector("#img_hero_1").classList.remove("img_rotate_right");
-   document.querySelector("#img_hero_2").classList.remove("img_rotate_left");
+   // document.querySelector("#img_hero_2").classList.remove("img_rotate_left");
 
    const animations = () => {
       setTimeout(() => animation_1(), timeTimeOut);
       setTimeout(() => animation_2(), timeTimeOut += time);
-      setTimeout(() => animation_3(), timeTimeOut += time);
-      setTimeout(() => animation_4(), timeTimeOut += time);
-      setTimeout(() => animation_5(), timeTimeOut += time);
-      setTimeout(() => animation_6(), timeTimeOut += time);
-      setTimeout(() => animation_7(), timeTimeOut += time);
+      setTimeout(() => animation_3(), timeTimeOut += time + 2500);
    }
    animations();
 })
